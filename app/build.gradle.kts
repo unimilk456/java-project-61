af1c4707829a47e3b9ac8400ce97970d0bce02ce
Mikhail-Davidovich <mikhail.davidovich@edetek.com>
2023-12-18 22:37:21 +0400

    remove kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,4 @@
 plugins {
-    id("java")
-    kotlin("jvm")
     application
     distribution
     checkstyle
@@ -19,14 +17,10 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
     useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
 }
 
 tasks.getByName("run", JavaExec::class) {
--- app/settings.gradle.kts
+++ app/settings.gradle.kts
@@ -1,8 +1,3 @@
-pluginManagement {
-    plugins {
-        kotlin("jvm") version "1.9.21"
-    }
-}
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
